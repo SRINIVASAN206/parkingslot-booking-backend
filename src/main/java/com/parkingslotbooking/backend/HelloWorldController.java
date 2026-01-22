d8c16d32203d2e776b77a5f5bae0086b34d3b7c7
srinivasan <nsseenu74@gmail.com>
2026-01-22 10:56:56 +0530

    database connected

--- src/main/java/com/parkingslotbooking/backend/HelloWorldController.java
+++ src/main/java/com/parkingslotbooking/backend/HelloWorldController.java
@@ -1,10 +1,12 @@
 package com.parkingslotbooking.backend;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
+@RequestMapping("/hello")
 public class HelloWorldController {
-    @GetMapping("/hello")
+    @GetMapping()
     String showhelloworld()
     {
         return "helloworld";

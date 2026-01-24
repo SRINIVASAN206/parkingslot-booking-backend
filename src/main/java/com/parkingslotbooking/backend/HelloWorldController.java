10d116635f16b7636967dd15896c896e52ce1b27
srinivasan <nsseenu74@gmail.com>
2026-01-24 11:34:17 +0530

    add new method

--- src/main/java/com/parkingslotbooking/backend/HelloWorldController.java
+++ src/main/java/com/parkingslotbooking/backend/HelloWorldController.java
@@ -4,12 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/api")
 public class HelloWorldController {
-    @GetMapping()
+    @GetMapping("/hello")
     String showhelloworld()
     {
         return "helloworld";
+    }
+
+    @GetMapping("/bye")
+    String showbyeworld()
+    {
+        return "byeworld";
     }
 
 }
